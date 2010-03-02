f34298833c1d67499f3479a9ffe4b6bb52a3ef5c
yongyuan.jiang <yongyuan.jiang@130000fe-de25-11de-ac01-4132a06e1c04>
2010-03-02 10:26:40 +0000

    新增正则表达式判断full gc

--- src/code/google/gclogviewer/GCLogAnalyze.java
+++ src/code/google/gclogviewer/GCLogAnalyze.java
@@ -16,123 +16,170 @@ import java.io.InputStreamReader;
 import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Analyze gc log to GCLogData
  * 
- * TODO: Refator to Strategy pattern,so can be extended to support multi jvm version and other jvm(like ibm)
+ * TODO: Refator to Strategy pattern,so can be extended to support multi jvm
+ * version and other jvm(like ibm)
  * 
  * @author <a href="mailto:bluedavy@gmail.com">BlueDavy</a>
  */
 public class GCLogAnalyze {
 
-	private static final String APPSTOP_WORD_PREFIX="Total time ";
-	
-	private static final int APPSTOP_WORD_PREFIX_LEN=
-		"Total time for which application threads were stopped: ".length();
-	
-	private static final int APPSTOP_WORD_SUFFIX_LEN=" seconds".length();
-	
-	private static final String FULLGC_KEYWORD="Full GC";
-	
-	private static final String MINORGC_KEYWORD1="ParNew";
-	
-	private static final String MINORGC_KEYWORD2="PSYoungGen";
-	
-	private static final String CMSGC_KEYWORD="CMS-";
-	
-	private static final String CMSGC_FAIL1="promotion failed";
-	
-	private static final String CMSGC_FAIL2="concurrent mode failure";
-	
-	public static final String CMSGC_TYPE="CMS";
-	
-	public static final String PARAGC_TYPE="Parallel";
-	
-	public GCLogData analysis(String fileName) throws Exception{
-		BufferedReader reader=new BufferedReader(new InputStreamReader(new FileInputStream(new File(fileName))));
-		String line=null;
-		GCLogData data=new GCLogData();
-		double sumAppStoppedTime=0;
-		double maxAppStoppedTime=0;
-		int cmsGCFail1Count=0,cmsGCFail2Count=0;
-		double maxGCLogTime=0;
-		while((line=reader.readLine())!=null){
-			line=line.trim();
+	private static final String APPSTOP_WORD_PREFIX = "Total time ";
+
+	private static final int APPSTOP_WORD_PREFIX_LEN = "Total time for which application threads were stopped: "
+			.length();
+
+	private static final int APPSTOP_WORD_SUFFIX_LEN = " seconds".length();
+
+	private static final String FULLGC_KEYWORD = "Full GC";
+
+	private static final String MINORGC_KEYWORD1 = "ParNew";
+
+	private static final String MINORGC_KEYWORD2 = "PSYoungGen";
+
+	private static final String CMSGC_KEYWORD = "CMS-";
+
+	private static final String CMSGC_FAIL1 = "promotion failed";
+
+	private static final String CMSGC_FAIL2 = "concurrent mode failure";
+
+	public static final String CMSGC_TYPE = "CMS";
+
+	public static final String PARAGC_TYPE = "Parallel";
+
+	// [GC [<collector>: <starting occupancy1> -> <ending occupancy1>, <pause
+	// time1> secs] <starting occupancy3> -> <ending occupancy3>, <pause time3>
+	// secs]
+	private Pattern mp = Pattern
+			.compile("\\] ([0-9]*)K->([0-9]*)K\\(([0-9]*)K\\)");
+
+	public GCLogData analysis(String fileName) throws Exception {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				new FileInputStream(new File(fileName))));
+		String line = null;
+		GCLogData data = new GCLogData();
+		double sumAppStoppedTime = 0;
+		double maxAppStoppedTime = 0;
+		int cmsGCFail1Count = 0, cmsGCFail2Count = 0;
+		double maxGCLogTime = 0;
+		while ((line = reader.readLine()) != null) {
+			line = line.trim();
 			// Full GC
-			if(line.indexOf(FULLGC_KEYWORD)!=-1){
-				double happenTime=Double.parseDouble(line.substring(0,line.indexOf(":")));
-				if(happenTime>maxGCLogTime)
-					maxGCLogTime=happenTime;
-				if(line.indexOf("real=")==-1){
-					System.out.println("ignore full gc info,maybe because it's not complete,line info: "+line);
-				}
-				else{
-					String consumeTime=line.substring(line.indexOf("real=")+5,line.length()-" secs]".length());
-					int memoryChangeInfoBegin=0;
-					if(line.indexOf(CMSGC_TYPE)!=-1){
-						memoryChangeInfoBegin=line.indexOf("]")+1;
-						data.setGCType(CMSGC_TYPE+" GC");
+			if (line.indexOf(FULLGC_KEYWORD) != -1) {
+				double happenTime = Double.parseDouble(line.substring(0, line
+						.indexOf(":")));
+				if (happenTime > maxGCLogTime)
+					maxGCLogTime = happenTime;
+				if (line.indexOf("real=") == -1) {
+					System.out
+							.println("ignore full gc info,maybe because it's not complete,line info: "
+									+ line);
+				} else {
+					String consumeTime = line.substring(
+							line.indexOf("real=") + 5, line.length()
+									- " secs]".length());
+					int memoryChangeInfoBegin = 0;
+					if (line.indexOf(CMSGC_TYPE) != -1) {
+						memoryChangeInfoBegin = line.indexOf("]") + 1;
+						data.setGCType(CMSGC_TYPE + " GC");
+					} else {
+						memoryChangeInfoBegin = line.indexOf("]", line
+								.indexOf("]") + 1) + 1;
+						data.setGCType(PARAGC_TYPE + " GC");
 					}
-					else{
-						memoryChangeInfoBegin=line.indexOf("]", line.indexOf("]")+1)+1;
-						data.setGCType(PARAGC_TYPE+" GC");
-					}
-					int memoryChangeInfoEnd=line.indexOf("(", memoryChangeInfoBegin);
-					try{
-						String memoryChangeInfo=line.substring(memoryChangeInfoBegin,memoryChangeInfoEnd).trim();
-						String[] memoryChangeInfos=memoryChangeInfo.split("->");
-						data.getFullGCConsumeTimes().put(String.valueOf(happenTime), consumeTime);
-						data.getFullGCMemoryChanges().put(String.valueOf(happenTime), memoryChangeInfos);
-					}
-					catch(StringIndexOutOfBoundsException e){
-						System.out.println("igore gull gc info,maybe because this line info not correct: "+line);
+					int memoryChangeInfoEnd = line.indexOf("(",
+							memoryChangeInfoBegin);
+					
+					Matcher matcher = mp.matcher(line);
+					if (matcher.find()) {
+						String before = matcher.group(1);
+						String after = matcher.group(2);
+						String total = matcher.group(3);
+
+						// 新的解析方法
+						String[] memoryChangeInfos = { before, after };
+						data.getFullGCConsumeTimes().put(
+								String.valueOf(happenTime), consumeTime);
+						data.getFullGCMemoryChanges().put(
+								String.valueOf(happenTime), memoryChangeInfos);
+					} else {
+						try {
+							String memoryChangeInfo = line.substring(
+									memoryChangeInfoBegin, memoryChangeInfoEnd)
+									.trim();
+							String[] memoryChangeInfos = memoryChangeInfo
+									.split("->");
+							data.getFullGCConsumeTimes().put(
+									String.valueOf(happenTime), consumeTime);
+							data.getFullGCMemoryChanges().put(
+									String.valueOf(happenTime),
+									memoryChangeInfos);
+						} catch (StringIndexOutOfBoundsException e) {
+							System.out
+									.println("igore gull gc info,maybe because this line info not correct: "
+											+ line);
+						}
 					}
 				}
 			}
 			// Minor GC
-			else if((line.indexOf(MINORGC_KEYWORD1)!=-1 || line.indexOf(MINORGC_KEYWORD2)!=-1)){
+			else if ((line.indexOf(MINORGC_KEYWORD1) != -1 || line
+					.indexOf(MINORGC_KEYWORD2) != -1)) {
 				// handle promotion failed special
-				if(line.indexOf(CMSGC_FAIL1)==-1){
-					double happenTime=Double.parseDouble(line.substring(0,line.indexOf(":")));
-					if(happenTime>maxGCLogTime)
-						maxGCLogTime=happenTime;
-					int memoryChangeInfoBegin=line.indexOf("]")+1;
-					int memoryChangeInfoEnd=line.indexOf(" secs]", memoryChangeInfoBegin);
-					String memoryChangeInfo=line.substring(memoryChangeInfoBegin,memoryChangeInfoEnd).trim();
-					String consumeTime=(memoryChangeInfo.split(",")[1]).trim();
-					memoryChangeInfo=memoryChangeInfo.split(",")[0];
-					memoryChangeInfo=memoryChangeInfo.substring(0,memoryChangeInfo.indexOf("("));
-					String[] memoryChangeInfos=memoryChangeInfo.split("->");
-					data.getMinorGCConsumeTimes().put(String.valueOf(happenTime), consumeTime);
-					data.getMinorGCMemoryChanges().put(String.valueOf(happenTime), memoryChangeInfos);
+				if (line.indexOf(CMSGC_FAIL1) == -1) {
+					double happenTime = Double.parseDouble(line.substring(0,
+							line.indexOf(":")));
+					if (happenTime > maxGCLogTime)
+						maxGCLogTime = happenTime;
+					int memoryChangeInfoBegin = line.indexOf("]") + 1;
+					int memoryChangeInfoEnd = line.indexOf(" secs]",
+							memoryChangeInfoBegin);
+					String memoryChangeInfo = line.substring(
+							memoryChangeInfoBegin, memoryChangeInfoEnd).trim();
+					String consumeTime = (memoryChangeInfo.split(",")[1])
+							.trim();
+					memoryChangeInfo = memoryChangeInfo.split(",")[0];
+					memoryChangeInfo = memoryChangeInfo.substring(0,
+							memoryChangeInfo.indexOf("("));
+					String[] memoryChangeInfos = memoryChangeInfo.split("->");
+					data.getMinorGCConsumeTimes().put(
+							String.valueOf(happenTime), consumeTime);
+					data.getMinorGCMemoryChanges().put(
+							String.valueOf(happenTime), memoryChangeInfos);
 				}
-				if(data.getGCType()==null){
-					if(line.indexOf(MINORGC_KEYWORD1)!=-1){
-						data.setGCType(PARAGC_TYPE+" GC");
+				if (data.getGCType() == null) {
+					if (line.indexOf(MINORGC_KEYWORD1) != -1) {
+						data.setGCType(PARAGC_TYPE + " GC");
 					}
 				}
 			}
 			// CMS GC
-			else if(line.indexOf(CMSGC_KEYWORD)!=-1){
-				
+			else if (line.indexOf(CMSGC_KEYWORD) != -1) {
+
 			}
 			// Application stopped time
-			else if(line.startsWith(APPSTOP_WORD_PREFIX)){
-				double stopTime=Double.parseDouble(line.substring(APPSTOP_WORD_PREFIX_LEN,line.length()-APPSTOP_WORD_SUFFIX_LEN));
-				sumAppStoppedTime+=stopTime;
-				if(stopTime>maxAppStoppedTime)
-					maxAppStoppedTime=stopTime;
+			else if (line.startsWith(APPSTOP_WORD_PREFIX)) {
+				double stopTime = Double.parseDouble(line.substring(
+						APPSTOP_WORD_PREFIX_LEN, line.length()
+								- APPSTOP_WORD_SUFFIX_LEN));
+				sumAppStoppedTime += stopTime;
+				if (stopTime > maxAppStoppedTime)
+					maxAppStoppedTime = stopTime;
 			}
-			
-			if(line.indexOf(CMSGC_FAIL1)!=-1){
+
+			if (line.indexOf(CMSGC_FAIL1) != -1) {
 				cmsGCFail1Count++;
-				data.setGCType(CMSGC_TYPE+" GC");
+				data.setGCType(CMSGC_TYPE + " GC");
 			}
-			
-			if(line.indexOf(CMSGC_FAIL2)!=-1){
+
+			if (line.indexOf(CMSGC_FAIL2) != -1) {
 				cmsGCFail2Count++;
-				data.setGCType(CMSGC_TYPE+" GC");
+				data.setGCType(CMSGC_TYPE + " GC");
 			}
 		}
 		data.setSumAppStoppedTime(sumAppStoppedTime);
@@ -142,35 +189,35 @@ public class GCLogAnalyze {
 		data.setGcLogTime(maxGCLogTime);
 		return data;
 	}
-	
-	class GCLogData{
-	
-		// key: time  value: consumetime
-		private Map<String, String> minorGCConsumeTimes=new HashMap<String, String>();
-		
+
+	class GCLogData {
+
+		// key: time value: consumetime
+		private Map<String, String> minorGCConsumeTimes = new HashMap<String, String>();
+
 		// key: time value: String[0]: memoryBefore String[1]: memoryAfter
-		private Map<String, String[]> minorGCMemoryChanges=new HashMap<String, String[]>();
-		
-		// key: time  value: consumetime
-		private Map<String, String> fullGCConsumeTimes=new HashMap<String, String>();
-		
+		private Map<String, String[]> minorGCMemoryChanges = new HashMap<String, String[]>();
+
+		// key: time value: consumetime
+		private Map<String, String> fullGCConsumeTimes = new HashMap<String, String>();
+
 		// key: time value: String[0]: memoryBefore String[1]: memoryAfter
-		private Map<String, String[]> fullGCMemoryChanges=new HashMap<String, String[]>();
-		
-		// key: time  value: consumetime
-		private Map<String, String> cmsGCConsumeTimes=new HashMap<String, String>();
-		
+		private Map<String, String[]> fullGCMemoryChanges = new HashMap<String, String[]>();
+
+		// key: time value: consumetime
+		private Map<String, String> cmsGCConsumeTimes = new HashMap<String, String>();
+
 		private double sumAppStoppedTime;
-		
+
 		private double maxAppStoppedTime;
-		
-		private int cmsGCFail1Count=0;
-		
-		private int cmsGCFail2Count=0;
-		
-		private double gcLogTime=0;
-		
-		private DecimalFormat doubleformat=new DecimalFormat("#0.00");
+
+		private int cmsGCFail1Count = 0;
+
+		private int cmsGCFail2Count = 0;
+
+		private double gcLogTime = 0;
+
+		private DecimalFormat doubleformat = new DecimalFormat("#0.00");
 
 		public Map<String, String> getMinorGCConsumeTimes() {
 			return minorGCConsumeTimes;
@@ -193,18 +240,20 @@ public class GCLogAnalyze {
 		}
 
 		public String getSumAppStoppedTime() {
-			return doubleformat.format(sumAppStoppedTime)+" seconds";
+			return doubleformat.format(sumAppStoppedTime) + " seconds";
 		}
 
 		public String getMaxAppStoppedTime() {
-			return doubleformat.format(maxAppStoppedTime)+" seconds";
+			return doubleformat.format(maxAppStoppedTime) + " seconds";
 		}
 
-		public void setMinorGCConsumeTimes(Map<String, String> minorGCConsumeTimes) {
+		public void setMinorGCConsumeTimes(
+				Map<String, String> minorGCConsumeTimes) {
 			this.minorGCConsumeTimes = minorGCConsumeTimes;
 		}
 
-		public void setMinorGCMemoryChanges(Map<String, String[]> minorGCMemoryChanges) {
+		public void setMinorGCMemoryChanges(
+				Map<String, String[]> minorGCMemoryChanges) {
 			this.minorGCMemoryChanges = minorGCMemoryChanges;
 		}
 
@@ -212,7 +261,8 @@ public class GCLogAnalyze {
 			this.fullGCConsumeTimes = fullGCConsumeTimes;
 		}
 
-		public void setFullGCMemoryChanges(Map<String, String[]> fullGCMemoryChanges) {
+		public void setFullGCMemoryChanges(
+				Map<String, String[]> fullGCMemoryChanges) {
 			this.fullGCMemoryChanges = fullGCMemoryChanges;
 		}
 
@@ -245,7 +295,7 @@ public class GCLogAnalyze {
 		}
 
 		public String getGcLogTime() {
-			return doubleformat.format(gcLogTime)+" s";
+			return doubleformat.format(gcLogTime) + " s";
 		}
 
 		public void setGcLogTime(double gcLogTime) {
@@ -253,63 +303,67 @@ public class GCLogAnalyze {
 		}
 
 		public String getThroughput() {
-			return doubleformat.format(100-sumAppStoppedTime*100/gcLogTime)+"%";
+			return doubleformat.format(100 - sumAppStoppedTime * 100
+					/ gcLogTime)
+					+ "%";
 		}
-		
-		public int getFullGCCount(){
+
+		public int getFullGCCount() {
 			return fullGCConsumeTimes.size();
 		}
-		
-		public String getFullGCConsumeTime(){
-			double consumeTimes=0;
+
+		public String getFullGCConsumeTime() {
+			double consumeTimes = 0;
 			for (String consumeTime : fullGCConsumeTimes.values()) {
-				consumeTimes+=Double.parseDouble(consumeTime);
+				consumeTimes += Double.parseDouble(consumeTime);
 			}
-			return doubleformat.format(consumeTimes)+" s";
+			return doubleformat.format(consumeTimes) + " s";
 		}
-		
-		public String getAvgFullGCConsumeTime(){
-			double consumeTimes=0;
+
+		public String getAvgFullGCConsumeTime() {
+			double consumeTimes = 0;
 			for (String consumeTime : fullGCConsumeTimes.values()) {
-				consumeTimes+=Double.parseDouble(consumeTime);
+				consumeTimes += Double.parseDouble(consumeTime);
 			}
-			return doubleformat.format(consumeTimes/getFullGCCount())+" s";
+			return doubleformat.format(consumeTimes / getFullGCCount()) + " s";
 		}
-		
-		public int getMinorGCCount(){
+
+		public int getMinorGCCount() {
 			return minorGCConsumeTimes.size();
 		}
-		
-		public String getMinorGCConsumeTime(){
-			double consumeTimes=0;
+
+		public String getMinorGCConsumeTime() {
+			double consumeTimes = 0;
 			for (String consumeTime : minorGCConsumeTimes.values()) {
-				consumeTimes+=Double.parseDouble(consumeTime);
+				consumeTimes += Double.parseDouble(consumeTime);
 			}
-			return doubleformat.format(consumeTimes)+" s";
+			return doubleformat.format(consumeTimes) + " s";
 		}
-		
-		public String getAvgMinorGCConsumeTime(){
-			double consumeTimes=0;
+
+		public String getAvgMinorGCConsumeTime() {
+			double consumeTimes = 0;
 			for (String consumeTime : minorGCConsumeTimes.values()) {
-				consumeTimes+=Double.parseDouble(consumeTime);
+				consumeTimes += Double.parseDouble(consumeTime);
 			}
-			return doubleformat.format(consumeTimes/getMinorGCCount())+" s";
+			return doubleformat.format(consumeTimes / getMinorGCCount()) + " s";
 		}
-		
-		public String getAvgStopTime(){
-			return doubleformat.format(sumAppStoppedTime*60*1000/gcLogTime)+" ms/m";
+
+		public String getAvgStopTime() {
+			return doubleformat.format(sumAppStoppedTime * 60 * 1000
+					/ gcLogTime)
+					+ " ms/m";
 		}
-		
+
 		private String gcType;
-		
-		public String getGCType(){
+
+		public String getGCType() {
 			return gcType;
 		}
-		
-		public void setGCType(String gcType){
-			this.gcType=gcType;
+
+		public void setGCType(String gcType) {
+			this.gcType = gcType;
 		}
-	
+
 	}
-	
+
 }
